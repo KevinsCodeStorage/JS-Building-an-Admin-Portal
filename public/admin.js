c951f6ef687043adbad41e6889844ffce5f158d5
KevinsCodeStorage <89494039+KevinsCodeStorage@users.noreply.github.com>
2021-11-15 21:26:29 -0500

    Initial in-class work

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,24 @@
-
 // Your Code Here
+
+async function getBooks(){
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+
+    books.forEach(adminDisplay)
+
+}
+
+function adminDisplay(book){
+    let adminContainer = document.querySelector('.bookUpdate')
+    adminContainer.innerHTML+=`
+        <li>
+            <label for="book">${book.title}:</label>
+            <input type="text" id="book" name="book" value=${book.quantity}>
+        </li>
+
+
+    `
+
+}
+
+getBooks()
