482bd396d3386a98e95ddaadbf92569c4aea3f51
KevinsCodeStorage <89494039+KevinsCodeStorage@users.noreply.github.com>
2021-11-17 10:27:43 -0500

    Admin Page now allows user to update the quantity of books on the API

--- public/admin.js
+++ public/admin.js
@@ -8,17 +8,36 @@ async function getBooks(){
 
 }
 
-function adminDisplay(book){
-    let adminContainer = document.querySelector('.bookUpdate')
-    adminContainer.innerHTML+=`
-        <li>
-            <label for="book">${book.title}:</label>
-            <input type="text" id="book" name="book" value=${book.quantity}>
-        </li>
 
+function adminDisplay(book) {
+    let root = document.querySelector('#root')
 
-    `
+    let li = document.createElement('li')
+    li.textContent = book.title
 
+    let quantityInput = document.createElement('input')
+    quantityInput.value = book.quantity
+
+    let saveButton = document.createElement('button')
+    saveButton.textContent = 'Save'
+
+    saveButton.addEventListener('click', () => {
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: quantityInput.value
+            })
+        })
+    })
+
+    li.append(quantityInput, saveButton)
+
+    root.append(li)
 }
+
 
 getBooks()
